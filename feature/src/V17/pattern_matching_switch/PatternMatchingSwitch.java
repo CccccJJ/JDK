cd91da4273ec1272c1670f5bcc0e365f4740fcd4
cc <1>
2023-12-22 10:23:58 +0800

    append V17 Pattern Matching Switch

---
+++ feature/src/V17/pattern_matching_switch/PatternMatchingSwitch.java
@@ -0,0 +1,19 @@
+package V17.pattern_matching_switch;
+
+public class PatternMatchingSwitch {
+
+    public static void main(String[] args) {
+        Object o = "Pattern Matching Switch";
+
+        matching(o);
+    }
+
+    public static void matching(Object o) {
+        switch (o) {
+            case String string && string.length() > 5 -> System.out.println("String & Length > 5");
+            case String string -> System.out.println("String");
+            case Integer integer -> System.out.println("Integer");
+            default -> System.out.println("not supported class type");
+        }
+    }
+}
---
+++ feature/src/V17/pattern_matching_switch/README.md
@@ -0,0 +1,3 @@
+Pattern Matching for Switch
+
+模式匹配是更加灵活的 switch 操作
