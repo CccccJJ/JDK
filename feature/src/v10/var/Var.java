589a1a26c6f0cd7e712f1bec2c13b83edba1d87c
cc <1>
2023-12-22 14:42:03 +0800

    modify v10 var generic & internfaces

--- feature/src/v10/var/Var.java
+++ feature/src/v10/var/Var.java
@@ -1,7 +1,26 @@
 package v10.var;
 
+import java.io.Closeable;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.RandomAccess;
+import java.util.concurrent.CopyOnWriteArrayList;
+
 public class Var {
     public static void main(String[] args) {
+        // case 1
         var a = 100;
+
+        // case 2
+        var listWithInterface = createListWithInterface(true);
+    }
+
+    public static <T extends List<E> & RandomAccess & Closeable & Serializable, E>
+    T createListWithInterface(boolean flag) {
+        if (flag) {
+            return (T) new ArrayList<E>();
+        }
+        return (T) new CopyOnWriteArrayList<E>();
     }
 }
