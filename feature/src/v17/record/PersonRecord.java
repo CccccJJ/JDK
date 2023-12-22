aa0e1d46e83f9af05c5ee9279eeff24f23d3396e
cc <1>
2023-12-22 11:09:17 +0800

    modify v17 record to enhance with construction

--- feature/src/v17/record/PersonRecord.java
+++ feature/src/v17/record/PersonRecord.java
@@ -2,4 +2,10 @@ package v17.record;
 
 public record PersonRecord(String name, Integer age) {
 
+    public PersonRecord {
+        if(age > 18){
+            throw new IllegalArgumentException("age cant not > 18");
+        }
+    }
+
 }
