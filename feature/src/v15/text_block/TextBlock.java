1525b5ae9677e767944853f427afb0b052a3ef2e
cc <1>
2023-12-22 13:41:29 +0800

    append v15 text block

---
+++ feature/src/v15/text_block/TextBlock.java
@@ -0,0 +1,15 @@
+package v15.text_block;
+
+public class TextBlock {
+    public static void main(String[] args) {
+        String html = """
+                <html>
+                  <body>
+                    <p> Hello, World </p>
+                  </body>
+                </html>
+                """;
+
+        System.out.println(html);
+    }
+}
